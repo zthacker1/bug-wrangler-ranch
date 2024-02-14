5104d480e0a5d46096b23aa8d5bcf4fb035e7c60
Steve Brownlee <stevebrownlee@users.noreply.github.com>
2024-02-14 15:09:05 -0600

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ console.log(`
 `)
 
 console.log(`You will be accompanying ${drovers.length} drovers as they drive ${cattleToDrive} cattle to Old Red's Ranch for grazing`)
-console.log(`\nThe herd is made of up the following types of cattle:`)
+console.log(`\nThe herd is made of up the following cattle (only their breed is shown):`)
 console.log(`${cattle}\n`)
 
 console.log("Here is the team of drovers you will be joining")
